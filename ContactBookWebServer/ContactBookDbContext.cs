750ad37ca6c8045f3b0559d57e1d5144eb588c0f
IFOP0112003 Кустов Дмитрий Вячеславович <IFOP0112003@sibupk.su>
2020-07-06 02:09:05 +0700

    First commit

--- ContactBookWebServer/ContactBookDbContext.cs
+++ ContactBookWebServer/ContactBookDbContext.cs
@@ -12,7 +12,7 @@ namespace ContactBookWebServer
         {
             Database.EnsureCreated();
         }
-
+        
         public DbSet<Contact> Contacts { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)

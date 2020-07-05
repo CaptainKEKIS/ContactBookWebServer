5d76a83ebaf492975521f933bf094b319b5c45fd
IFOP0112003 Кустов Дмитрий Вячеславович <IFOP0112003@sibupk.su>
2020-07-06 02:08:32 +0700

    Добавьте файлы проекта.

---
+++ ContactBookWebServer/ContactBookDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactBookWebServer
+{
+    public class ContactBookDbContext : DbContext
+    {
+        public ContactBookDbContext(DbContextOptions<ContactBookDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Database=ContactsDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+    }
+}
